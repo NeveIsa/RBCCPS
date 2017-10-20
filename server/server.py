470880bf988a889e9bd02220d6f384c4a71f17a1
neveisa <smpdmohanty@gmail.com>
2017-10-20 22:08:16 +0530

    auto commit

--- server/server.py
+++ server/server.py
@@ -1,11 +1,49 @@
-from flask import Flask
+import os
+from flask import Flask,redirect
+import uuid
+
+
+
+gformurl=open("gformurl.txt").read()
+import requests
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+gform_doc=requests.get(gformurl,headers=headers).text
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(gform_doc, 'html.parser')
+
+for x in soup.findAll("script"):
+  x.decompose()
+
+with open("gform.html",'w') as g:
+  g.write(str(soup))
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    status=open("status.txt").read()
-    return open("index.html").read().format(status)
+def index():
+  if os.path.exists("recordID.txt"):
+    recordID=open("recordID.txt").read()
+  else:
+    recordID=str(uuid.uuid4())
+    with open("recordID.txt",'w') as g:
+      g.write(recordID)
 
+
+  status=open("status.txt").read().strip()
+  recordIDscript="<script>var recordID='{0}'\nvar jobStatus='{1}'</script>".format(recordID,status)
+  template=open("header.html").read()+open("gform.html").read()+recordIDscript+open("footer.html").read()
+ 
+  
+  return template
+  
+@app.route("/currentJob")
+def showCurrentJob():
+  recordID=open("recordID.txt").read()
+  return "<style>body{background-image: url('http://idolza.com/a/f/b/backgrounds-cool-wallpaper-cave-stunning-graphicsheat_cool-designs-wallpaper_grey-paint-for-bedroom-nice-kitchen-colors-bathrooms-design-christmas-light-decorating-ideas-triangle-island-mode.jpg');}</style><h2 style='color:white;'>Current job has started with Job ID: %s </h2>" % recordID
+
+#@app.route("/record")
+#def record(recordID):
 
 
 app.run(port=80)
