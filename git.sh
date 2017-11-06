eef72f56af7fcf53acd175bf8f7da3104b51e1e4
neveisa <smpdmohanty@gmail.com>
2017-11-06 11:51:02 +0530

    auto commit

--- git.sh
+++ git.sh
@@ -1,6 +1,7 @@
 prev_dir=`pwd`
 BASEDIR=$(dirname "$0")
 tput setaf 1;echo "changing dir to $BASEDIR"
+tput sgr0;
 cd $BASEDIR
 
 git add .
@@ -9,3 +10,4 @@ git push
 
 tput setaf 1;echo "changing dir to $prev_dir"
 cd $prev_dir
+tput sgr0;
