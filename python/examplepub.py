0be1e2c049d2cdc668abc7ba1975493328922ceb
neveisa <smpdmohanty@gmail.com>
2017-11-10 11:55:08 +0530

    auto commit

---
+++ python/examplepub.py
@@ -0,0 +1,27 @@
+
+## SAMPLE CODE TAKEN FROM
+## https://www.hivemq.com/blog/mqtt-client-library-paho-python
+
+import paho.mqtt.client as paho
+import time
+
+def on_publish(client, userdata, mid):
+    print("mid: "+str(mid))
+
+client = paho.Client()
+client.on_publish = on_publish
+client.connect("localhost", 1883)
+client.loop_start()
+
+
+TEMP_COUNT=0
+def read_from_imaginary_thermometer():
+    global TEMP_COUNT
+    TEMP_COUNT+=1
+    return TEMP_COUNT
+
+
+while True:
+    temperature = read_from_imaginary_thermometer()
+    (rc, mid) = client.publish("mwdata", str(temperature), qos=1)
+    time.sleep(1)
