0199eeeb4fe7cf999c8523a35b00738d9f6147c0
Richard <richard@localhost.localdomain>
2017-12-02 21:16:08 +0530

    auto commit

---
+++ evongo/settings.py
@@ -0,0 +1,15 @@
+DOMAIN = {'middlewaredata': {}}
+
+MONGO_DBNAME  = 'helloworld'
+
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+ALLOW_UNKNOWN=True
