f0a75c206b2fc12d3ad6ac67b0d03361ee9f8c85
Richard <richard@localhost.localdomain>
2017-12-06 15:27:28 +0530

    auto commit

--- evongo/tools/mwpacket.py
+++ evongo/tools/mwpacket.py
@@ -1,1 +1,32 @@
 import json
+import datetime
+
+class datapacket:
+  def __init__(self,deviceid,datatype,dataunit,extract=False):
+    self.deviceid=deviceid
+    self.datatype=datatype
+    self.dataunit=dataunit
+    self.extract=extract
+
+  def getpacket(self,rawdata):
+    try:
+      rawdata=json.loads(rawdata)
+    except:
+      print("Invalid JSON...")
+
+    if self.extract and 'timestamp' in rawdata:
+      payload['timestamp']=rawdata['timestamp']
+    else:
+      payload['timestamp']=datetime.datetime.utcnow().isoformat()
+
+    payload['data']=rawdata
+    payload['deviceid']=self.deviceid
+    payload['datatype']=self.datatype
+    payload['dataunit']=self.dataunit
+
+    return payload
+
+
+if __name__=="__main__":
+  d=datapacket("name","type","unit")
+  print d.getpacket('{"hello":"world"}')
