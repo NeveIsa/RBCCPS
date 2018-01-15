0618b712362c4cdd7d228c6225bd135d31be7075
neveisa <smpdmohanty@gmail.com>
2018-01-15 15:38:45 +0530

    auto commit

--- RUBY/app.rb
+++ RUBY/app.rb
@@ -6,11 +6,41 @@ require 'sinatra'
 
 conf=File.open("mosconf.json").read
 conf=JSON.parse(conf)
-model=Model.new(conf['passwordfile'],conf['aclsfile'])
+mosquitto_conf_file=conf["mosquitto_conf_file"]
+
+puts "========"*7
+puts "MOSQUITTO CONF. FILE: " + mosquitto_conf_file
+puts "========"*7
+
+aclsfile=""
+passwordfile=""
+
+File.open(mosquitto_conf_file).each do |line|
+	if /password_file (.+)/.match(line)
+		passwordfile,=/password_file (.+)/.match(line).captures
+		#puts passwordfile
+	end	
+	if /acl_file (.+)/.match(line)
+		aclsfile,=/acl_file (.+)/.match(line).captures
+		#puts aclsfile
+	end
+end
+
+if aclsfile=="" or passwordfile==""
+	puts "============="
+	puts mosquitto_conf_file + " doesn't contain acl_file or password_file entry in it"		
+	puts "============="
+	exit
+end
+
+
 
 puts "==========="*7
-puts conf
+puts "Found ==> \n #{passwordfile}\n #{aclsfile} \n"
 puts "==========="*7
+
+
+model=Model.new(passwordfile,aclsfile)
 
 get '/' do
 	content_type :json
