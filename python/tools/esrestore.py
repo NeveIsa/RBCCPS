e2be321cbb3b094b8854e890c69641cbdb82620e
neveisa <smpdmohanty@gmail.com>
2018-05-07 15:27:44 +0530

    auto commit

--- python/tools/esrestore.py
+++ python/tools/esrestore.py
@@ -1,12 +1,12 @@
 import esbackup
+import time
 
 repo=esbackup.Repo()
 repo.register()
 
 repo.restore()
 
-while true
-do
-sleep 1
-repo.check_restore_progress()
-done
+while True:
+  time.sleep(1)
+  repo.check_restore_progress()
+
