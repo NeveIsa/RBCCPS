3e6d9ede4aa4c7e1704d65dbbc9b13b90823ac1e
richard <richard@localhost.localdomain>
2018-05-08 02:29:48 -0400

    auto commit

---
+++ tools/fragmentation.sh
@@ -0,0 +1,21 @@
+index=$1 
+
+cd /mnt/extHD/RAW_SEGREGATED/
+
+
+ls $index && mkdir $index
+if [ $? -ne 0]
+then
+echo "Folder exists already..."
+exit 1
+fi
+
+cd $index
+
+sort -t, -nk2 /mnt/extHD/RAW/data/$index -o "$index_sorted"
+
+for device in "screenprinter"
+header=$(head -1 $index_sorted) > "$device.csv"
+awk "/$device/" $index_sorted >>  "$device.csv"
+
+
