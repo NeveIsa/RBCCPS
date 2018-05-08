6d83cf4a9c982a2493da3f9797b8f366b326820c
richard <richard@localhost.localdomain>
2018-05-08 05:15:37 -0400

    auto commit

--- tools/fragmentation.sh
+++ tools/fragmentation.sh
@@ -1,21 +1,65 @@
-index=$1 
+index=$1
 
+if [[ $1 == "" ]]
+then 
+echo "No index name supplied... exiting"
+exit 1
+else 
+echo "Supplied index ---> $index"
+fi 
+sleep 2
+
+echo ""
+echo "Checking if Index exists -- $index"
+rawFile="/mnt/extHD/RAW/data/$index"
+ls $rawFile
+if [ $? -ne 0 ]
+then
+echo "The supplied file with index name $index doesn't exist"
+exit 1
+else
+echo "Found index $index"
+fi
+sleep 2
+
+
+echo ""
+echo "changing dir to /mnt/extHD/RAW_SEGREGATED/"
 cd /mnt/extHD/RAW_SEGREGATED/
+sleep 2
 
+echo ""
+echo "Creating dir named $index"
 
 mkdir $index
 if [ $? -ne 0 ]
 then
-echo "Folder exists already..."
+echo ""
+echo "Folder exists already... EXITING"
 exit 1
+else
+echo "Successfully created folder $index"
 fi
+sleep 2
 
+
+echo ""
+echo "Changing dir to $index"
 cd $index
+sleep 2
 
-sort -t, -nk2 /mnt/extHD/RAW/data/$index -o "$index_sorted"
+echo ""
+echo "Current Directory ---> `pwd`"
 
-for device in "screenprinter"
-header=$(head -1 $index_sorted) > "$device.csv"
-awk "/$device/" $index_sorted >>  "$device.csv"
 
+for device in "loader" "screenprinter" "pickandplace1" "pickandplace2" "reflowoven" "bakingoven1" "bakingoven2"
+do
+	echo ""
+	head -1 $rawFile > temp
+	echo "Grepping.... $device"
+	awk "/$device/" $rawFile >> temp
+	echo "Sorting... $device"
+	sort -t, -nk2 temp -o "$device.csv"
+	rm temp
+done
 
