7a5e370fe8fa25b35dbbf3be53045fc609578475
IIOT_PRIMARY <you@example.com>
2018-05-27 22:02:46 +0000

    auto commit

--- csvDownloader/getcsv.sh
+++ csvDownloader/getcsv.sh
@@ -1,6 +1,18 @@
 server=10.156.14.143
 
-if [ "$1" = "ls" ]
+
+curl $server/dataLNK/ > .temp
+cat .temp | grep -Po "helloworld-\d+-\d+-\d+" | sort -u 
+
+
+echo ""
+
+
+if [ "$1" = "" ]
 then
-curl $server
+echo "Pass a filename to download"
+else
+wget $server/dataLNK/$1
 fi
+
+rm .temp
