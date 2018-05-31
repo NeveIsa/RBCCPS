afc582d965eb75070e62e2b2ea5e6b6fcbc22561
iiot_secondary <richard@localhost.localdomain>
2018-05-31 10:27:26 -0400

    auto commit

--- ServerInit/init.sh
+++ ServerInit/init.sh
@@ -8,3 +8,10 @@ wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.4.rp
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.4.rpm.sha512
 shasum -a 512 -c elasticsearch-6.2.4.rpm.sha512
 sudo rpm --install elasticsearch-6.2.4.rpm
+
+
+wget https://artifacts.elastic.co/downloads/kibana/kibana-6.2.4-x86_64.rpm
+shasum -a 512 kibana-6.2.4-x86_64.rpm 
+sudo rpm --install kibana-6.2.4-x86_64.rpm
+
+
