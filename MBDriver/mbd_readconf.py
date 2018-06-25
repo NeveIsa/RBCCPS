74fd070739f932df06b91c214c5264c3799e9242
neveisa <smpdmohanty@gmail.com>
2018-06-25 18:45:08 +0530

    auto commit

--- MBDriver/mbd_readconf.py
+++ MBDriver/mbd_readconf.py
@@ -6,6 +6,10 @@ import time
 import datetime
 import json
 import yaml
+from pprint import pprint
+
+# Validate YAML per conf file
+import cerberus
 
 CONFIG_DIR = "conf.d"
 LAST_SCANNED = 0
@@ -26,25 +30,39 @@ def findConfFiles(_dir):
         
         
     
-def isFileUpdated(_filename):
-    fPath = CONFIG_DIR+"/"+_filename
-    if LAST_SCANNED < os.path.getmtime(fPath):
+def isFileUpdated(_file):
+    if LAST_SCANNED < os.path.getmtime(_file):
         return True
     else:
         return False
     
-def readConfigFile(_filename):
-    fPath = CONFIG_DIR+"/"+_filename
-    with open(fPath) as f:
+def readConfigFile(_file):
+    with open(_file) as f:
         conf = f.read()
     try:
-        conf=json.loads(conf)
+        conf=yaml.load(conf)
         return conf
     except Exception as e:
         print(e)
     
             
+def validateConfigFile(_file):
+    with open("schema.yaml") as f:
+        schema=f.read()
     
+    with open(_file) as f:
+        conf=f.read()
+    
+    schema=yaml.load(schema)
+    pprint(schema)
+    print ("\n====\n"*2)
+    conf=yaml.load(conf)
+
+    v=cerberus.Validator(allow_unknown=False)
+    print(v.validate(conf,schema))
+
+    pprint(v.errors)
+
 
 def main():
     global LAST_SCANNED, MODBUS_CONFIG
@@ -52,6 +70,8 @@ def main():
     tempTime = int(time.time())
     print("Files found:\n--> "+"\n--> ".join(confFiles)+"\n" )
     for curFile in confFiles:
+        curFile = CONFIG_DIR+"/"+ curFile
+        validateConfigFile(curFile)
         if isFileUpdated(curFile):
             print("Found modified:", curFile)
             MODBUS_CONFIG[curFile]=readConfigFile(curFile)
@@ -61,11 +81,11 @@ def main():
             
             
     LAST_SCANNED = tempTime
-    print(MODBUS_CONFIG)
+    pprint(MODBUS_CONFIG)
     
 if __name__ =="__main__":
     while (1):
         main()
         print("====="*10)
+        #print("====="*10)
         time.sleep(10)
-        
